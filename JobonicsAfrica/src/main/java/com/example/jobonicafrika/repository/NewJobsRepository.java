a60d7e65d5883e4622c2d28bd6707a144eb0bafb
nelsonkimaiga <nelsonkimaiga@aol.com>
2019-02-02 21:00:44 +0300

    remove duplicate directory

--- JobonicsAfrica/src/main/java/com/example/jobonicafrika/repository/NewJobsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.jobonicafrika.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.jobonicafrika.model.NewJobs;
-
-public interface NewJobsRepository extends JpaRepository<NewJobs, Integer> {
-
-}
